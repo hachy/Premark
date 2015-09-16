0ae9d20387f7c6e5f9e18db1cf57df5e9bc2253f
hachy <1613863+hachy@users.noreply.github.com>
2015-09-16 13:19:46 +0900

    use es6

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-'use strict';
+import ipc from 'ipc'
+import process from 'process'
+import fs from 'fs'
+import marked from 'marked'
 
-var remote = require('remote');
-var ipc = require('ipc');
-var process = remote.require('process');
-var fs = remote.require('fs');
-var marked = require('marked');
+ipc.on('read-md', (filepath) => {
+  read(filepath)
+})
 
-ipc.on('read-md', function(filepath) {
-  read(filepath);
-});
-
-ipc.on('open-md', function(filepath) {
-  read(filepath);
-  watch(filepath);
-});
+ipc.on('open-md', (filepath) => {
+  read(filepath)
+  watch(filepath)
+})
 
 marked.setOptions({
-  highlight: function(code) {
-    return require('highlight.js').highlightAuto(code).value;
+  highlight: (code) => {
+    return require('highlight.js').highlightAuto(code).value
   }
-});
+})
 
 function read(filepath) {
-  fs.readFile(filepath, 'utf-8', function(err, data) {
+  fs.readFile(filepath, 'utf-8', (err, data) => {
     if (err) {
-      process.stderr.write('No such file or directory: ' + filepath);
-      ipc.send('err', 'error');
-      return;
+      process.stderr.write(`No such file or directory:  ${filepath}`)
+      ipc.send('err', 'error')
+      return
     }
-    document.getElementById('markdown').innerHTML = marked(data);
-  });
+    document.getElementById('markdown').innerHTML = marked(data)
+  })
 }
 
 function watch(filepath) {
-  fs.watchFile(filepath, { interval: 10 }, function() {
-    read(filepath);
-  });
+  fs.watchFile(filepath, { interval: 10 }, () => {
+    read(filepath)
+  })
 }
--- src/main.js
+++ src/main.js
@@ -1,78 +1,78 @@
-'use strict';
+import app from 'app'
+import BrowserWindow from 'browser-window'
+import Menu from 'menu'
+import path from 'path'
+import ipc from 'ipc'
+import dialog from 'dialog'
+import reporter from 'crash-reporter'
+reporter.start()
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var Menu = require('menu');
-var path = require('path');
-var ipc = require('ipc');
+let mainWindow = null
 
-require('crash-reporter').start();
+const appName = app.getName()
 
-var mainWindow = null;
-var appName = app.getName();
+app.on('window-all-closed', () => {
+  app.quit()
+})
 
-app.on('window-all-closed', function() {
-  app.quit();
-});
+ipc.on('err', (event, arg) => {
+  const readme = path.resolve(__dirname, '..', 'README.md')
+  mainWindow.setTitle(appName)
+  mainWindow.webContents.send('read-md', readme)
+})
 
-ipc.on('err', function(event, arg) {
-  var readme = path.resolve(__dirname, '..', 'README.md');
-  mainWindow.setTitle(appName);
-  mainWindow.webContents.send('read-md', readme);
-});
-
-app.on('ready', function() {
-  Menu.setApplicationMenu(menu);
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+app.on('ready', () => {
+  Menu.setApplicationMenu(menu)
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadUrl(`file://${__dirname}/index.html`)
 
   if (process.argv.length >= 2) {
-    var filepath = process.argv[process.argv.length - 1];
-    mainWindow.webContents.on('did-finish-load', function() {
-      setWinTitle(filepath);
-      mainWindow.webContents.send('open-md', filepath);
-    });
+    const filepath = process.argv[process.argv.length - 1]
+    mainWindow.webContents.on('did-finish-load', () => {
+      setWinTitle(filepath)
+      mainWindow.webContents.send('open-md', filepath)
+    })
   } else if (process.argv.length <= 1) {
-    var readme = path.resolve(__dirname, '..', 'README.md');
-    mainWindow.webContents.on('did-finish-load', function() {
-      mainWindow.webContents.send('read-md', readme);
-    });
+    const readme = path.resolve(__dirname, '..', 'README.md')
+    mainWindow.webContents.on('did-finish-load', () => {
+      mainWindow.webContents.send('read-md', readme)
+    })
   }
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
 
-app.on('open-file', function(event, filepath) {
-  event.preventDefault();
-  mainWindow.webContents.send('open-md', filepath);
-});
+app.on('open-file', (event, filepath) => {
+  event.preventDefault()
+  mainWindow.webContents.send('open-md', filepath)
+})
 
 function setWinTitle(fn) {
-  var title = path.basename(fn);
-  mainWindow.setTitle(title + ' - ' + appName);
+  const title = path.basename(fn)
+  mainWindow.setTitle(`${title} - ${appName}`)
 }
 
-var template = [
+const template = [
   {
     label: 'File',
     submenu: [
       {
         label: 'Open',
         accelerator: 'Command+O',
-        click: function() {
-          require('dialog').showOpenDialog({
+        click: () => {
+          dialog.showOpenDialog({
             properties: ['openFile'],
             filters: [{ name: 'All Files', extensions: ['markdown', 'md']}]
-          }, function(filenames) {
+          }, (filenames) => {
             if (!filenames || !filenames[0]) {
-              return;
+              return
             }
-            var filepath = filenames[0];
-            setWinTitle(filepath);
-            mainWindow.webContents.send('open-md', filepath);
-          });
+            const filepath = filenames[0]
+            setWinTitle(filepath)
+            mainWindow.webContents.send('open-md', filepath)
+          })
         }
       }
     ]
@@ -93,14 +93,14 @@ var template = [
       }
     ]
   }
-];
+]
 
 if (process.platform === 'darwin') {
   template.unshift({
     label: appName,
     submenu: [
       {
-        label: 'About ' + appName,
+        label: `About ${appName}`,
         role: 'about'
       },
       {
@@ -109,10 +109,10 @@ if (process.platform === 'darwin') {
       {
         label: 'Quit',
         accelerator: 'Command+Q',
-        click: function() { app.quit(); }
+        click: () => { app.quit() }
       }
     ]
-  });
+  })
 }
 
-var menu = Menu.buildFromTemplate(template);
+const menu = Menu.buildFromTemplate(template)
